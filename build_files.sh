0c3b20f67059691a2baefd9f5e78d5c7eedae911
Molokoedov Mikhail <molokoedovmp@gmail.com>
2024-06-06 13:08:33 +0500

    dep 3

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,2 @@
-echo "BUILD START"
-pip install -r requirements.txt
-python manage.py makemigrations
 python manage.py migrate
-echo "BUIKD END"
+python manage.py collectstatic --noinput
--- eventcalendar/settings.py
+++ eventcalendar/settings.py
@@ -160,7 +160,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles_build"),)
 #STATIC_ROOT=os.path.join(BASE_DIR, 'static/')
 
 #
