3dea6eba6ae1e9d4e3dbd676999f4921de632cb8
Molokoedov Mikhail <molokoedovmp@gmail.com>
2024-06-08 15:38:38 +0500

    dep 1.8

--- eventcalendar/settings.py
+++ eventcalendar/settings.py
@@ -104,9 +104,9 @@ USE_L10N = True
 
 USE_TZ = False
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
